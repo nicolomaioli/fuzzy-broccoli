d4b156f457b1e78519290b2ec8b3c1e86d1ca181
Nicolo Maioli <nicolomaioli@gmail.com>
2023-07-24 14:59:39 +0100

    Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+/**
+ * Documentation: https://swapi.dev/documentation#people
+ */
+const url = 'https://swapi.dev/api/people/1';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
